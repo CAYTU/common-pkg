bd9236ef505d7c8f0458362480db3f90d0f10e55
jmndao <jmndao@gmail.com>
2022-09-13 12:56:22 +0000

    :sparkles: feat: tasks|zone|agents-srv subjects events added

--- src/nats-events/subjects.ts
+++ src/nats-events/subjects.ts
@@ -10,4 +10,16 @@ export enum Subjects {
   RobotCreated = "robot:created",
   RobotUpdated = "robot:updated",
   RobotDeleted = "robot:deleted",
+  // Taks Subjects
+  TaskCreated = "task:created",
+  TaskUpdated = "task:updated",
+  TaskDeleted = "task:deleted",
+  // Zone Subjects
+  ZoneCreated = "zone:created",
+  ZoneUpdated = "zone:updated",
+  ZoneDeleted = "zone:deleted",
+  // Agent Subjects
+  AgentCreated = "agent:created",
+  AgentUpdated = "agent:updated",
+  AgentDeleted = "agent:deleted",
 }

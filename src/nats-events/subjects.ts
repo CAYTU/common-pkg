44d04581daf1b0243df7eaf2202c5587916430d2
jmndao <jmndao@gmail.com>
2022-09-13 10:37:45 +0000

    :sparkles: feat: Customers subjects events added

--- src/nats-events/subjects.ts
+++ src/nats-events/subjects.ts
@@ -2,4 +2,8 @@ export enum Subjects {
   ProductCreated = "product:created",
   ProductUpdated = "product:updated",
   ProductDeleted = "product:deleted",
+  // Customer Subjects
+  CustomerCreated = "customer:created",
+  CustomerUpdated = "customer:updated",
+  CustomerDeleted = "customer:deleted",
 }

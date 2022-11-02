98715ef8eb643927f268717064ee5f94de166866
jmndao <jmndao@gmail.com>
2022-11-02 13:21:18 +0000

    Changed auth middleware to fit with OAuth 2.0 - Bearer token

--- src/middlewares/user/current-user.ts
+++ src/middlewares/user/current-user.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 
 interface UserPayload {
   id: string;
+  username: string;
   email: string;
   roles: string[];
 }
@@ -18,20 +19,33 @@ declare global {
 
 export const currentUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.session?.jwt) {
-      return next();
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      try {
+        // Split the headers then grap token at index 1
+        token = req.headers.authorization.split(" ")[1];
+        // Validate the user token with the Secret key
+        const payload = jwt.verify(
+          token,
+          `${process.env.ACCESS_TOKEN_PRIVATE_KEY}`
+        ) as UserPayload;
+
+        // Save decoded payload in req.currentUser
+        req.currentUser = payload;
+      } catch (err) {
+        console.error(err);
+        res.status(401);
+        throw new Error("Not Authorized, token failed");
+      }
+      next();
     }
 
-    try {
-      const payload = jwt.verify(
-        req.session?.jwt,
-        `${process.env.JWT_SECRET}`
-      ) as UserPayload;
-
-      req.currentUser = payload;
-
-    } catch (err) {}
-
-    next();
+    if (!token) {
+      next();
+    }
   }
 );

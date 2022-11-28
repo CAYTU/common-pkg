0bcd8abbbec8735d4e7a2219037011734e866ea5
jmndao <jmndao@gmail.com>
2022-11-28 16:06:15 +0000

    change token name:

--- src/utils/gen-jwt/index.ts
+++ src/utils/gen-jwt/index.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateJWT = <T>(data: T) => {
-  return jwt.sign(data as {}, `${process.env.ACCESS_TOKEN_PRIVATE_KEY}`, {
+  return jwt.sign(data as {}, `${process.env.PAYLOAD_PRIVATE_KEY}`, {
     expiresIn: "1d",
   });
 };

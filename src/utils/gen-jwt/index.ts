4e4b250f924c70d1b24a4237975e239d41b16ffb
jmndao <jmndao@gmail.com>
2022-11-28 16:07:59 +0000

    export generateJWT func

--- src/utils/gen-jwt/index.ts
+++ src/utils/gen-jwt/index.ts
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const generateJWT = <T>(data: T) => {
+export const generateJWT = <T>(data: T) => {
   return jwt.sign(data as {}, `${process.env.PAYLOAD_PRIVATE_KEY}`, {
     expiresIn: "1d",
   });
 };
-
-export default generateJWT;

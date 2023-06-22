914262a39ac4ec4798cc4622fbe8a60706f121e6
jmndao <jmndao@gmail.com>
2023-06-22 13:06:20 +0000

    refactor and document rbac

--- src/middlewares/auth/role-base-access.ts
+++ src/middlewares/auth/role-base-access.ts
@@ -1,15 +1,22 @@
 import asyncHandler from "express-async-handler";
 import { NextFunction, Request, Response } from "express";
 import { ForbiddenErr } from "../../errors/forbidden";
+import { UserRole } from "../../types/utils";
 
-// Role Based Access: User ACL
+/**
+ * Role Based Access Control (RBAC) middleware for user access control.
+ */
 const RbaUserACL = {
+  /**
+   * Middleware that allows access for users with UserRole.Create, UserRole.All, or UserRole.Admin roles.
+   * Throws a ForbiddenErr if access is denied.
+   */
   canCreate: asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       if (
-        req.currentUser?.roles.includes("create") ||
-        req.currentUser?.roles.includes("all") ||
-        req.currentUser?.roles.includes("admin")
+        req.currentUser?.roles?.includes(UserRole.Create) ||
+        req.currentUser?.roles?.includes(UserRole.All) ||
+        req.currentUser?.roles?.includes(UserRole.Admin)
       ) {
         next();
       } else {
@@ -18,40 +25,52 @@ const RbaUserACL = {
     }
   ),
 
+  /**
+   * Middleware that allows access for users with "edit", UserRole.All, or UserRole.Admin roles.
+   * Throws a ForbiddenErr if access is denied.
+   */
   canEdit: asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       if (
-        req.currentUser?.roles.includes("edit") ||
-        req.currentUser?.roles.includes("all") ||
-        req.currentUser?.roles.includes("admin")
+        req.currentUser?.roles?.includes(UserRole.Update) ||
+        req.currentUser?.roles?.includes(UserRole.All) ||
+        req.currentUser?.roles?.includes(UserRole.Admin)
       ) {
         next();
       } else {
-        throw new ForbiddenErr("Cannot Edit.");
+        throw new ForbiddenErr("Cannot edit.");
       }
     }
   ),
 
+  /**
+   * Middleware that allows access for users with UserRole.Delete, UserRole.All, or UserRole.Admin roles.
+   * Throws a ForbiddenErr if access is denied.
+   */
   canDelete: asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       if (
-        req.currentUser?.roles.includes("delete") ||
-        req.currentUser?.roles.includes("all") ||
-        req.currentUser?.roles.includes("admin")
+        req.currentUser?.roles?.includes(UserRole.Delete) ||
+        req.currentUser?.roles?.includes(UserRole.All) ||
+        req.currentUser?.roles?.includes(UserRole.Admin)
       ) {
         next();
       } else {
-        throw new ForbiddenErr("Cannot Delete.");
+        throw new ForbiddenErr("Cannot delete.");
       }
     }
   ),
 
+  /**
+   * Middleware that allows read-only access for users with "readOnly", UserRole.All, or UserRole.Admin roles.
+   * Throws a ForbiddenErr if access is denied.
+   */
   canReadOnly: asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       if (
-        req.currentUser?.roles.includes("readOnly") ||
-        req.currentUser?.roles.includes("all") ||
-        req.currentUser?.roles.includes("admin")
+        req.currentUser?.roles?.includes(UserRole.ReadOnly) ||
+        req.currentUser?.roles?.includes(UserRole.All) ||
+        req.currentUser?.roles?.includes(UserRole.Admin)
       ) {
         next();
       } else {
@@ -60,44 +79,56 @@ const RbaUserACL = {
     }
   ),
 
+  /**
+   * Middleware that allows access for users with UserRole.Customer, UserRole.Admin, UserRole.All, or UserRole.Operator roles.
+   * Throws a ForbiddenErr if access is denied.
+   */
   isCustomer: asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       if (
-        req.currentUser?.roles.includes("customer") ||
-        req.currentUser?.roles.includes("admin") ||
-        req.currentUser?.roles.includes("all") ||
-        req.currentUser?.roles.includes("operator")
+        req.currentUser?.roles?.includes(UserRole.Customer) ||
+        req.currentUser?.roles?.includes(UserRole.Admin) ||
+        req.currentUser?.roles?.includes(UserRole.All) ||
+        req.currentUser?.roles?.includes(UserRole.Operator)
       ) {
         next();
       } else {
-        throw new ForbiddenErr("Operation is not allowed for customer.");
+        throw new ForbiddenErr("Operation is not allowed for customers.");
       }
     }
   ),
 
+  /**
+   * Middleware that allows access for users with UserRole.Operator, UserRole.Admin, or UserRole.All roles.
+   * Throws a ForbiddenErr if access is denied.
+   */
   isOperator: asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       if (
-        req.currentUser?.roles.includes("operator") ||
-        req.currentUser?.roles.includes("admin") ||
-        req.currentUser?.roles.includes("all")
+        req.currentUser?.roles?.includes(UserRole.Operator) ||
+        req.currentUser?.roles?.includes(UserRole.Admin) ||
+        req.currentUser?.roles?.includes(UserRole.All)
       ) {
         next();
       } else {
-        throw new ForbiddenErr("Operation is not allowed for operator.");
+        throw new ForbiddenErr("Operation is not allowed for operators.");
       }
     }
   ),
 
+  /**
+   * Middleware that allows access for users with UserRole.Admin or UserRole.All roles.
+   * Throws a ForbiddenErr if access is denied.
+   */
   isAdmin: asyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
       if (
-        req.currentUser?.roles.includes("all") ||
-        req.currentUser?.roles.includes("admin")
+        req.currentUser?.roles?.includes(UserRole.Admin) ||
+        req.currentUser?.roles?.includes(UserRole.All)
       ) {
         next();
       } else {
-        throw new ForbiddenErr("Operation is not allowed for admin.");
+        throw new ForbiddenErr("Operation is not allowed for admins.");
       }
     }
   ),

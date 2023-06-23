f2abce2fdecf0291e54c2ed9073da9e33adfdfd2
jmndao <jmndao@gmail.com>
2023-06-23 19:28:39 +0000

    add message.field optional

--- src/errors/not-found.ts
+++ src/errors/not-found.ts
@@ -3,13 +3,12 @@ import { CustomErr } from "./custom";
 export class NotFoundErr extends CustomErr {
   statusCode = 404;
 
-  constructor() {
-    super("Route not Found");
-
+  constructor(public msg?: string) {
+    super(msg);
     Object.setPrototypeOf(this, NotFoundErr.prototype);
   }
 
   serializeErrors(): { message: string; field?: string | undefined }[] {
-    return [{ message: "Not Found" }];
+    return [{ message: this.msg ?? "Not Found Error." }];
   }
 }

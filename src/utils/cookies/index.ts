3bf50b470455e57a84a603ff4d50b0a94ea59187
jmndao <jmndao@gmail.com>
2023-06-23 18:51:36 +0000

    add check safe

--- src/utils/cookies/index.ts
+++ src/utils/cookies/index.ts
@@ -1,12 +1,37 @@
-// Retrieve cookies from the request object
-
+/**
+ * Retrieves a cookie value from the request object.
+ * @param {Object} req - The request object.
+ * @param {string} name - The name of the cookie.
+ * @returns {string|undefined} The cookie value, or undefined if the cookie is not found.
+ */
 export const getCookie = (req: any, name: string) => {
-  const cookie =
-    req.headers.cookie
-      ?.split(";")
-      .find((c: any) => c.trim().startsWith(`${name}=`)) || req.cookies[name];
+  /**
+   * The cookies object containing the cookies from the request headers or cookies.
+   * @type {string}
+   */
+  const cookies = req?.headers?.cookie || req?.cookies;
+
+  if (!cookies) {
+    return undefined;
+  }
+
+  /**
+   * The cookie value matching the provided name.
+   * @type {string|undefined}
+   */
+  const cookie = cookies
+    .split(";")
+    .find((c: any) => c.trim().startsWith(`${name}=`));
+
   if (!cookie) {
     return undefined;
   }
-  return cookie.split("=")[1];
+
+  /**
+   * The extracted value from the cookie.
+   * @type {string}
+   */
+  const [, value] = cookie.split("=");
+
+  return value;
 };

013bef08d1b8b90683219dc6e47d2cc74e471430
jmndao <jmndao@gmail.com>
2023-09-03 18:55:29 +0000

    refactor: Enhance Publisher class for dual event publishing
    Updated the Publisher class to enable event publishing to both NATS and WebSocket clients. Introduced improved error handling and WebSocket client checking to ensure robust event transmission.

--- src/nats-events/publisher.ts
+++ src/nats-events/publisher.ts
@@ -9,18 +9,43 @@ interface Event {
 export abstract class Publisher<T extends Event> {
   abstract subject: T["subject"];
 
-  private client: Stan;
+  private natsClient: Stan;
+  private wsClient: any; // Adjust the type based on your WebSocket client
 
-  constructor(client: Stan) {
-    this.client = client;
+  constructor(natsClient: Stan, wsClient?: any) {
+    this.natsClient = natsClient;
+    this.wsClient = wsClient;
   }
 
-  publish(data: T["data"]): Promise<void> {
+  async publish(data: T["data"]): Promise<void> {
+    await this.publishNats(data);
+    await this.publishWs(data);
+  }
+
+  private publishNats(data: T["data"]): Promise<void> {
     return new Promise((resolve, reject) => {
-      this.client.publish(this.subject, JSON.stringify(data), (err) => {
+      this.natsClient.publish(this.subject, JSON.stringify(data), (err) => {
         if (err) {
           reject(err);
         }
+        resolve();
+      });
+    });
+  }
+
+  private publishWs(data: T["data"]): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (!this.wsClient) {
+        console.error("(WS) WebSocket client is not defined.");
+        return reject(new Error("WebSocket client is not defined."));
+      }
+
+      this.wsClient.publish(this.subject, JSON.stringify(data), (err: any) => {
+        if (err) {
+          return reject(err);
+        }
+
+        console.log("(WS) Event published to subject", this.subject);
         resolve();
       });
     });

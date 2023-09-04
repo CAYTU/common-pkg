1c9c8a7a67c1eba526bd7d08f4cea03e9d3e9f8b
jmndao <jmndao@gmail.com>
2023-09-04 13:27:24 +0000

    splitted publisher to nats and ws

--- src/nats-events/publisher.ts
+++ src/nats-events/publisher.ts
@@ -1,47 +1,71 @@
 import { Stan } from "node-nats-streaming";
 import { Subjects } from "./subjects";
 
+/**
+ * An interface that defines the structure of an event.
+ */
 interface Event {
   subject: Subjects;
   data: any;
 }
 
+/**
+ * An abstract base class for publishing events to multiple communication channels.
+ * @typeparam T - The event type that extends the Event interface.
+ */
 export abstract class Publisher<T extends Event> {
+  /**
+   * The subject of the event to be published.
+   */
   abstract subject: T["subject"];
 
+  /**
+   * The NATS Streaming client used for publishing to NATS.
+   */
   private natsClient: Stan;
-  private wsClient: any; // Adjust the type based on your WebSocket client
 
+  /**
+   * The WebSocket client used for publishing to WebSocket.
+   * Adjust the type based on your WebSocket client implementation.
+   */
+  private wsClient: any;
+
+  /**
+   * Creates a new Publisher instance.
+   * @param natsClient - The NATS Streaming client instance.
+   * @param wsClient - (Optional) The WebSocket client instance for WebSocket communication.
+   */
   constructor(natsClient: Stan, wsClient?: any) {
     this.natsClient = natsClient;
     this.wsClient = wsClient;
   }
 
-  async publish(data: T["data"]): Promise<void> {
-    try {
-      await this.publishNats(data);
-      await this.publishWs(data);
-    } catch (error) {
-      console.error("Error publishing event:", error);
-      throw error; // Re-throw the error to propagate it up the stack
-    }
-  }
-
-  private publishNats(data: T["data"]): Promise<void> {
-    return new Promise((resolve, reject) => {
+  /**
+   * Publishes the event data to the NATS channel.
+   * @param data - The event data to be published.
+   * @returns A Promise that resolves when the event is successfully published or rejects if there is an error.
+   */
+  async publishToNats(data: T["data"]): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
       this.natsClient.publish(this.subject, JSON.stringify(data), (err) => {
         if (err) {
           console.error("Error publishing to NATS:", err);
-          reject(err); // Reject the promise with the error
+          reject(err);
         } else {
+          console.log("(NATS) Event published to subject", this.subject);
           resolve();
         }
       });
     });
   }
 
-  private publishWs(data: T["data"]): Promise<void> {
-    return new Promise((resolve, reject) => {
+  /**
+   * Publishes the event data to the WebSocket channel.
+   * @param data - The event data to be published.
+   * @returns A Promise that resolves when the event is successfully published or rejects if there is an error.
+   */
+  async publishToWs(data: T["data"]): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
       if (!this.wsClient) {
         console.error("(WS) WebSocket client is not defined.");
         reject(new Error("WebSocket client is not defined."));
@@ -51,7 +75,7 @@ export abstract class Publisher<T extends Event> {
       this.wsClient.publish(this.subject, JSON.stringify(data), (err: any) => {
         if (err) {
           console.error("Error publishing to WebSocket:", err);
-          reject(err); // Reject the promise with the error
+          reject(err);
         } else {
           console.log("(WS) Event published to subject", this.subject);
           resolve();

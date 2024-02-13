b76147ffea2f798758c929f4e8bda4a152074d3b
jmndao <jmndao@gmail.com>
2024-02-13 08:10:41 +0000

    refactor: Simplify RBAC middleware to directly use req.currentUser
    Updated RBAC middleware to directly use req.currentUser object instead of passing roles as parameters. Also streamlined logic for checking user connection to their organization.

--- src/middlewares/auth/role-base-access.ts
+++ src/middlewares/auth/role-base-access.ts
@@ -11,294 +11,310 @@ const RbaUserACL = {
    * Middleware that allows access for users with UserRole.Create, UserRole.All, or UserRole.Admin roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  canCreate: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  canCreate: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.All ||
-            role === UserRole.Admin ||
-            role === UserRole.SuperAdmin
+        userRoles?.some((role) =>
+          [
+            UserRole.Create,
+            UserRole.All,
+            UserRole.Admin,
+            UserRole.SuperAdmin,
+          ].includes(role)
         )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Cannot create.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows access for users with UserRole.Update, UserRole.All, or UserRole.Admin roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  canEdit: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  canEdit: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.All ||
-            role === UserRole.Admin ||
-            role === UserRole.SuperAdmin
+        userRoles?.some((role) =>
+          [
+            UserRole.Update,
+            UserRole.All,
+            UserRole.Admin,
+            UserRole.SuperAdmin,
+          ].includes(role)
         )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Cannot edit.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows access for users with UserRole.Delete, UserRole.All, or UserRole.Admin roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  canDelete: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  canDelete: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.All ||
-            role === UserRole.Admin ||
-            role === UserRole.SuperAdmin
+        userRoles?.some((role) =>
+          [
+            UserRole.Delete,
+            UserRole.All,
+            UserRole.Admin,
+            UserRole.SuperAdmin,
+          ].includes(role)
         )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Cannot delete.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows read-only access for users with UserRole.ReadOnly, UserRole.All, or UserRole.Admin roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  canReadOnly: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  canReadOnly: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.All ||
-            role === UserRole.Admin ||
-            role === UserRole.SuperAdmin
+        userRoles?.some((role) =>
+          [
+            UserRole.ReadOnly,
+            UserRole.All,
+            UserRole.Admin,
+            UserRole.SuperAdmin,
+          ].includes(role)
         )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Cannot read content.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows access for users with UserRole.Customer, UserRole.Admin, UserRole.All, or UserRole.Operator roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  isCustomer: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  isCustomer: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.All ||
-            role === UserRole.Admin ||
-            role === UserRole.SuperAdmin
+        userRoles?.some((role) =>
+          [
+            UserRole.Customer,
+            UserRole.Admin,
+            UserRole.All,
+            UserRole.Operator,
+            UserRole.SuperAdmin,
+          ].includes(role)
         )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Operation is not allowed for customers.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows access for users with UserRole.Operator, UserRole.Admin, or UserRole.All roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  isOperator: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  isOperator: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.All ||
-            role === UserRole.Admin ||
-            role === UserRole.SuperAdmin
+        userRoles?.some((role) =>
+          [
+            UserRole.Operator,
+            UserRole.Admin,
+            UserRole.All,
+            UserRole.SuperAdmin,
+          ].includes(role)
         )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Operation is not allowed for operators.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows access for users with UserRole.Admin or UserRole.All roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  isAdmin: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  isAdmin: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.All ||
-            role === UserRole.Admin ||
-            role === UserRole.SuperAdmin
+        userRoles?.some((role) =>
+          [UserRole.Admin, UserRole.All, UserRole.SuperAdmin].includes(role)
         )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Operation is not allowed for admins.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows access for users with UserRole.SuperAdmin or UserRole.All roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  isSuperAdmin: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  isSuperAdmin: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some((role) => userRoles?.includes(role) || role === UserRole.All)
+        userRoles?.some((role) =>
+          [UserRole.SuperAdmin, UserRole.All].includes(role)
+        )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Operation is not allowed for super admins.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows access for users with UserRole.Developer or UserRole.All roles.
    * Throws a ForbiddenErr if access is denied.
    */
-  isDeveloper: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  isDeveloper: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.All ||
-            role === UserRole.Developer ||
-            role === UserRole.SuperAdmin
+        userRoles?.some((role) =>
+          [UserRole.Developer, UserRole.All, UserRole.SuperAdmin].includes(role)
         )
       ) {
         next();
       } else {
         throw new ForbiddenErr("Operation is not allowed for developers.");
       }
-    }),
+    }
+  ),
 
   /**
    * Middleware that allows access for users with UserRole.Robot.
    * Throws a ForbiddenErr if access is denied.
    */
-  isRobot: (...roles: UserRole[]) =>
-    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  isRobot: asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // Check if the user is connected to their own organization
       const connectedToOwnOrganization =
-        req.currentUser?.currentOrganizationId ===
-        req.currentUser?.ownedOrganizationId;
+        !req.currentUser?.currentOrganizationId ||
+        req.currentUser.currentOrganizationId ===
+          req.currentUser.ownedOrganizationId;
 
       const userRoles = connectedToOwnOrganization
-        ? req.currentUser?.rolesInCurrentOrganization?.roles
-        : req.currentUser?.roles;
+        ? req.currentUser?.roles
+        : req.currentUser?.rolesInCurrentOrganization?.roles;
 
-      // Check if user has any of the provided roles
       if (
-        roles.some(
-          (role) =>
-            userRoles?.includes(role) ||
-            role === UserRole.SuperAdmin ||
-            role === UserRole.All
-        )
+        userRoles?.some((role) => [UserRole.Robot, UserRole.All].includes(role))
       ) {
         next();
       } else {
         throw new ForbiddenErr("Operation is not allowed for robots.");
       }
-    }),
+    }
+  ),
 };
 
 export { RbaUserACL };

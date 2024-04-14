c350701a7c0f719fa40ece08e2f930f738060479
jmndao <jmndao@gmail.com>
2024-04-14 17:41:06 +0000

    Add OrganizationMemberInterface to models

--- src/types/models/index.ts
+++ src/types/models/index.ts
@@ -521,6 +521,41 @@ declare namespace CTypes {
   }
 
   /**
+   * Represents an organization membership object with extended properties.
+   */
+  interface OrganizationMemberInterface extends IMongooseObjectExt {
+    /**
+     * The email of the user. Use for invitation.
+     */
+    email: string;
+
+    /**
+     * The unique identifier of the organization to which the user is invited.
+     */
+    organizationId: string;
+
+    /**
+     * The role of the user in the organization.
+     */
+    rolesInOrganization: UserRole[];
+
+    /**
+     * The type of tasks the user is allowed to execute within the organization
+     */
+    allowedTasksInOrganization: TaskType[];
+
+    /**
+     * The status of the membership.
+     */
+    status: UserMembershipStatus;
+
+    /**
+     * The unique identifier of the user. Populated when the user accepts the invitation.
+     */
+    user?: Types.ObjectId;
+  }
+
+  /**
    * Payment
    */
   export interface PaymentInterface extends IMongooseObjectExt {

63724d5ea2850665cf9cb5860c5ebcc0ceed0b40
jmndao <jmndao@gmail.com>
2024-05-06 11:07:31 +0000

    Refactor encryptor functions to accept expiresIn parameter

--- src/utils/encryptor/index.ts
+++ src/utils/encryptor/index.ts
@@ -24,30 +24,36 @@ export interface UserPayload extends JwtPayload {
 export const encryptor = {
   // This function will be used to encrypt an ACCESS_TOKEN
   // It will take a user object and return a token
-  encryptAccessToken: (user: UserPayload): string => {
-    return jwt.sign(user, `${process.env.ACCESS_SECRET}`, {
-      expiresIn: "1h",
+  encryptAccessToken: (
+    paylaod: any,
+    expiresIn: string | number | undefined
+  ): string => {
+    return jwt.sign(paylaod, `${process.env.ACCESS_SECRET}`, {
+      expiresIn: expiresIn ?? "1h",
     });
   },
 
   // This function will be used to encrypt a REFRESH_TOKEN
   // It will take a user object and return a token
-  encryptRefreshToken: (user: UserPayload): string => {
-    return jwt.sign(user, `${process.env.REFRESH_SECRET}`, {
-      expiresIn: "7d",
+  encryptRefreshToken: (
+    paylaod: any,
+    expiresIn: string | number | undefined
+  ): string => {
+    return jwt.sign(paylaod, `${process.env.REFRESH_SECRET}`, {
+      expiresIn: expiresIn ?? "7d",
     });
   },
 
   // This function will be used to decrypt an ACCESS_TOKEN
   // It will take a token and return a user object
-  decryptAccessToken: (token: string): UserPayload => {
-    return jwt.verify(token, `${process.env.ACCESS_SECRET}`) as UserPayload;
+  decryptAccessToken: (token: string): any => {
+    return jwt.verify(token, `${process.env.ACCESS_SECRET}`) as any;
   },
 
   // This function will be used to decrypt a REFRESH_TOKEN
   // It will take a token and return a user object
-  decryptRefreshToken: (token: string): UserPayload => {
-    return jwt.verify(token, `${process.env.REFRESH_SECRET}`) as UserPayload;
+  decryptRefreshToken: (token: string): any => {
+    return jwt.verify(token, `${process.env.REFRESH_SECRET}`) as any;
   },
 
   // This function will be used to verify an ACCESS_TOKEN and return a boolean

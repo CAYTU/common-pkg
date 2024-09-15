6d02840b888a289b4c73f74a1c866536f5164f2d
jmndao <jmndao@gmail.com>
2024-09-15 12:59:07 +0000

    chore: Update exports for errors, middlewares, and Nats events

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,11 @@ import CTypes from "./types/models/users";
 // Event imports
 import { ET } from "./types/events";
 
-export * from "./errors/bad-request";
-export * from "./errors/custom";
-export * from "./errors/not-authorized";
-export * from "./errors/not-found";
-export * from "./errors/request-validation";
-export * from "./errors/bad-gateway";
-export * from "./errors/database-fetch";
-export * from "./errors/forbidden";
-export * from "./errors/server-error";
-
-export * from "./middlewares/auth/authorize";
-export * from "./middlewares/auth/role-base-access";
-export * from "./middlewares/errors/db-connection";
-export * from "./middlewares/errors/handler";
-export * from "./middlewares/errors/validate-request";
-export * from "./middlewares/user/current-user";
-export * from "./middlewares/user/roles-generator";
 export * from "./middlewares/tasks/allowed-task";
 
 export * from "./middlewares/robots/ack-robot";
 export * from "./middlewares/robots/authorize-robot";
 
-export * from "./nats-events/listener";
-export * from "./nats-events/publisher";
-export * from "./nats-events/subjects";
 export * from "./types/utils";
 
 export * from "./utils/encryptor";
@@ -48,7 +28,17 @@ export * from "./billings/events";
  */
 export * from "./users/models";
 export * from "./users/events";
-// export * from "./users/utils";
+
+// Export Errors
+export * from "./errors";
+
+// Export Middlewares
+export * from "./middlewares/auth";
+export * from "./middlewares/errors";
+export * from "./middlewares/user";
+
+// Export Nats Events
+export * from "./nats-events";
 
 export { CTypes };
 export { ET };
